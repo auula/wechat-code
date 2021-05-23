4cd093bd968a7554d330ec97b419ef6e2fd7dac4
Dings <deen.job@qq.com>
2021-05-23 17:46:12 +0800

    updated struct.

--- escape/struct.go
+++ escape/struct.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	f := foo("Ding")
+	// ptr := &f
+	// fmt.Printf("main() %p \n", *ptr)
 	fmt.Println(f)
 }
 
@@ -11,20 +13,27 @@ type bar struct {
 	s string
 }
 
-func foo(s string) *bar {
+func foo(s string) bar {
 	f := new(bar)
+	ptr := &f
 	defer func() {
-		f = nil
+		//fmt.Printf("foo().func() %p \n", ptr)
+		*ptr = nil
+		//fmt.Printf("foo().func() %p \n", ptr)
 	}()
 	f.s = s
-	return f
+	//fmt.Printf("foo() %p \n", ptr)
+	return **ptr
 }
 
 // func foo(s string) bar {
 // 	f := new(bar)
+// 	ptr := &f
 // 	defer func() {
+// 		//fmt.Printf("foo().func() %p \n", ptr)
 // 		f = nil
 // 	}()
 // 	f.s = s
-// 	return *f
+// 	//fmt.Printf("foo() %p \n", ptr)
+// 	return **ptr
 // }
