ddeea00713883b1fbbd6b3d6b8846d5ad0a46b37
Dings <deen.job@qq.com>
2021-05-23 18:44:34 +0800

    delete struct binary file

--- escape/struct.go
+++ escape/struct.go
@@ -13,27 +13,27 @@ type bar struct {
 	s string
 }
 
+// func foo(s string) *bar {
+// 	f := new(bar)
+// 	ptr := &f
+// 	defer func() {
+// 		//fmt.Printf("foo().func() %p \n", ptr)
+// 		*ptr = nil
+// 		//fmt.Printf("foo().func() %p \n", ptr)
+// 	}()
+// 	f.s = s
+// 	//fmt.Printf("foo() %p \n", ptr)
+// 	return *ptr
+// }
+
 func foo(s string) bar {
 	f := new(bar)
 	ptr := &f
 	defer func() {
 		//fmt.Printf("foo().func() %p \n", ptr)
-		*ptr = nil
-		//fmt.Printf("foo().func() %p \n", ptr)
+		f = nil
 	}()
 	f.s = s
 	//fmt.Printf("foo() %p \n", ptr)
 	return **ptr
 }
-
-// func foo(s string) bar {
-// 	f := new(bar)
-// 	ptr := &f
-// 	defer func() {
-// 		//fmt.Printf("foo().func() %p \n", ptr)
-// 		f = nil
-// 	}()
-// 	f.s = s
-// 	//fmt.Printf("foo() %p \n", ptr)
-// 	return **ptr
-// }
